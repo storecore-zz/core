c77fbaa60e82fa941703bb07b3b0b69f42d17101
Ward van der Put <Ward.van.der.Put@gmail.com>
2015-03-18 15:44:35 +0100

    Add framework bootloader

---
+++ StoreCore/bootloader.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * StoreCore Framework Bootloader
+ *
+ * @author    Ward van der Put <Ward.van.der.Put@gmail.com>
+ * @copyright Copyright (c) 2015 StoreCore
+ * @license   http://www.gnu.org/licenses/gpl.html GPLv3
+ * @version   0.1.0
+ */
+
+ // Load, instantiate, and register the StoreCore autoloader
+require_once __DIR__ . 'Autoloader.php';
+$loader = new \StoreCore\Autoloader();
+$loader->register();
