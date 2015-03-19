c81a1feb3f98b8f42237379185f46ed0874d3d4a
Ward van der Put <Ward.van.der.Put@gmail.com>
2015-03-19 10:31:09 +0100

    Add configuration files

--- index.php
+++ index.php
@@ -9,3 +9,20 @@
  
 // Load configuration
 require_once 'version.php';
+
+if (is_file('config.php')) {
+    require 'config.php';
+} else {
+    $configuration = parse_ini_file('config.ini', false);
+    foreach ($configuration as $name => $value) {
+        $name = str_ireplace('.', '_', $name);
+        $name = strtoupper($name);
+        define($name, $value);
+    }
+    unset($configuration, $name, $value);
+}
+
+if (!defined('STORECORE_FILESYSTEM_LIBRARY_ROOT')) {
+    define('STORECORE_FILESYSTEM_LIBRARY_ROOT', __DIR__ . DIRECTORY_SEPARATOR . 'StoreCore');
+}
+require rtrim(STORECORE_FILESYSTEM_LIBRARY_ROOT, '\\/') . DIRECTORY_SEPARATOR . 'bootloader.php';
