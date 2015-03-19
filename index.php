aa9779bd32d1118208ed2cfdb34cc5945d0ef87a
Ward van der Put <Ward.van.der.Put@gmail.com>
2015-03-19 07:43:52 +0100

    Add global version ID

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * StoreCore Store Front Application
+ *
+ * @copyright Copyright (c) 2015 StoreCore
+ * @license   http://www.gnu.org/licenses/gpl.html GPLv3
+ * @version   0.1.0
+ */
+ 
+// Load configuration
+require_once 'version.php';
---
+++ version.php
@@ -0,0 +1,2 @@
+<?php
+define('STORECORE_VERSION', '0.1.0');
