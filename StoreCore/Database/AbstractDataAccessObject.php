094a2267065426f9f2efe6c1e52a8d197f3124cc
Ward van der Put <Ward.van.der.Put@gmail.com>
2015-03-20 14:55:31 +0100

    Add load balancing to the abstract DAO

--- StoreCore/Database/AbstractDataAccessObject.php
+++ StoreCore/Database/AbstractDataAccessObject.php
@@ -12,13 +12,29 @@ namespace StoreCore\Database;
  */
 abstract class AbstractDataAccessObject
 {
+    /** @type string VERSION */
+    const VERSION = '0.0.1';
+
+    /** @type object PDO */
     private $Connection;
-    
+
+    /**
+     * Connect on construction.
+     *
+     * @param void
+     * @return void
+     */
     public function __construct()
     {
         $this->connect();
     }
-    
+ 
+    /**
+     * Connect to the database.
+     *
+     * @param void
+     * @return void
+     */
     private function connect()
     {
         $dsn = STORECORE_DATABASE_DRIVER . ':dbname=' . STORECORE_DATABASE_DEFAULT_DATABASE
@@ -27,17 +43,39 @@ abstract class AbstractDataAccessObject
             $this->Connection = new \PDO($dsn, STORECORE_DATABASE_USERNAME, STORECORE_DATABASE_PASSWORD);
         } catch (\PDOException $e) {
             $logger = new \StoreCore\FileSystem\Logger();
-            $logger->error('Connection failed: ' . $e->getMessage());
+            $logger->error('Database connection failed: ' . trim($e->getMessage()));
+
+            // Reconnect after a few seconds to balance heavy loads
+            $seconds = mt_rand(2, 5);
+            $logger->notice('Reconnecting in ' . $seconds . ' seconds.');
+            sleep($seconds);
+            try {
+                $this->Connection = new \PDO($dsn, STORECORE_DATABASE_USERNAME, STORECORE_DATABASE_PASSWORD);
+            } catch (\PDOException $e) {
+                $logger->critical('Service unavailable: ' . trim($e->getMessage()));
+                if (!headers_sent()) {
+                    header('HTTP/1.1 503 Service Unavailable', true, 503);
+                    header('Retry-After: 60');
+                }
+                exit;
+            }
         }
     }
  
+    /**
+     * Fetch one or more rows from a single database table.
+     *
+     * @param string $value
+     * @param string|null $key
+     * @return array
+     */
     public function fetch($value, $key = null)
     {
         if ($key == null) {
             $key = $this->PrimaryKey;
         }
 
-        $sql = 'SELECT * FROM ' . $this->TableName . ' WHERE ' . $key . '=';
+        $sql = 'SELECT * FROM ' . $this->TableName . ' WHERE ' . $key . ' = ';
         if (is_int($value)) {
             $sql .= $value;
         } else {
