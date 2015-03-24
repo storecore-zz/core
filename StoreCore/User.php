e6459f69ce874c27cd88630104dba8709a150dbe
Ward van der Put <Ward.van.der.Put@gmail.com>
2015-03-24 21:59:06 +0100

    Add PHPDoc

--- StoreCore/User.php
+++ StoreCore/User.php
@@ -3,17 +3,30 @@ namespace StoreCore;
 
 class User
 {
+    /** @type string VERSION */
     const VERSION = '0.0.1';
 
+    /**
+     * @type int    $UserID
+     * @type string $Username
+     */
     private $UserID;
     private $Username;
 
+    /**
+     * @param int $user_id
+     * @return $this
+     */
     public function setUserID($user_id)
     {
         $this->UserID = (int)$user_id;
         return $this;
     }
-    
+
+    /**
+     * @param string $username
+     * @return $this
+     */
     public function setUsername($username)
     {
         $this->Username = $username;
