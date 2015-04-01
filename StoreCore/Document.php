850bd5ba255ddfd752d0d249b4d558a02f184118
Ward van der Put <Ward.van.der.Put@gmail.com>
2015-04-01 14:13:38 +0200

    Add Document core class and assets

---
+++ StoreCore/Document.php
@@ -0,0 +1,205 @@
+<?php
+namespace StoreCore;
+
+/**
+ * HTML5 Document
+ *
+ * @author    Ward van der Put <Ward.van.der.Put@gmail.com>
+ * @copyright Copyright (c) 2015 StoreCore
+ * @license   http://www.gnu.org/licenses/gpl.html
+ * @version   0.0.1
+ */
+class Document
+{
+    /**
+     * @type string $Direction
+     * @type string $Language
+     * @type null|array $Links
+     * @type null|array $MetaProperties
+     * @type array $Sections
+     * @type string $Title
+     */
+    protected $Direction = 'ltr';
+    protected $Language = 'en-GB';
+    protected $Links;
+    protected $MetaProperties;
+    protected $Sections = array();
+    protected $Title = 'StoreCore';
+
+    /**
+     * @type array $MetaData
+     */
+    protected $MetaData = array(
+        'robots' => 'index,follow',
+    );
+
+    /**
+     * @type string VERSION
+     *     Semantic Version (SemVer)
+     */
+    const VERSION = '0.0.1';
+    
+    /**
+     * @param string $title
+     * @return void
+     */
+    public function __construct($title = null)
+    {
+        if ($title !== null) {
+            $this->setTitle($title);
+        }
+    }
+
+    /**
+     * @param void
+     * @return string
+     */
+    public function __toString()
+    {
+        return $this->getDocument();
+    }
+
+    /**
+     * @param string $href
+     * @param string $rel
+     * @param string $type
+     * @param string $media
+     * @return $this
+     *
+     * @see https://developer.mozilla.org/en-US/docs/Web/HTML/Element/link
+     */
+    public function addLink($href, $rel = null, $type = null, $media = null)
+    {
+        $href = str_ireplace('https://', '//', $href);
+        $href = str_ireplace('http://', '//', $href);
+        $link = array('href' => $href);
+        
+        if ($rel !== null) {
+            $rel = strtolower($rel);
+            $link['rel'] = $rel;
+        }
+
+        if ($type !== null) {
+            $type = strtolower($type);
+            $link['type'] = $type;
+        }
+
+        if ($media !== null) {
+            $media = strtolower($media);
+            $link['media'] = $media;
+        }
+        
+        $this->Links[md5($href)] = $link;
+        return $this;
+    }
+
+    /**
+     * @param string $name
+     * @param string $content
+     * @return $this
+     */
+    public function addMetaData($name, $content)
+    {
+        $name = trim($name);
+        $name = strtolower($name);
+        $this->MetaData[$name] = $content;
+        return $this;
+    }
+
+    /**
+     * @param string $property
+     * @param string $content
+     * @return $this
+     */
+    public function addMetaProperty($property, $content)
+    {
+        $name = trim($property);
+        $name = strtolower($property);
+        $this->MetaProperties[$property] = $content;
+        return $this;
+    }
+
+    /**
+     * @param string $content
+     * @param string $container
+     * @return $this
+     */
+    public function addSection($content, $container = 'section')
+    {
+        $container = strtolower($container);
+
+        $this->Sections[] = '<' . $container . '>' . $content . '</' . $container . '>';
+        
+        return $this;
+    }
+
+    /**
+     * @param void
+     * @return string
+     */
+    public function getBody()
+    {
+        $body  = '<body>';
+        $body .= implode($this->Sections);
+        $body .= '</body>';
+        return $body;
+    }
+    /**
+     * @param void
+     * @return string
+     */
+    public function getDocument()
+    {
+        $html  = '<!DOCTYPE html>';
+        $html .= '<html dir="' . $this->Direction . '" lang="' . $this->Language . '">';
+        $html .= $this->getHead();
+        $html .= $this->getBody();
+        $html .= '</html>';
+        return $html;
+    }
+
+    /**
+     * @param void
+     * @return string
+     */
+    public function getHead()
+    {
+        $head  = '<head>';
+        $head .= '<meta charset="UTF-8">';
+        $head .= '<title>' . $this->Title . '</title>';
+
+        if ($this->Links !== null) {
+            foreach ($this->Links as $link) {
+                $head .= '<link';
+                foreach ($link as $attribute => $value) {
+                    $head .= ' ' . $attribute . '="' . $value . '"';
+                }
+                $head .= '>';
+            }
+        }
+
+        foreach ($this->MetaData as $name => $content) {
+            $head .= '<meta name="' . $name . '" content="' . $content . '">';
+        }
+
+        if ($this->MetaProperties !== null) {
+            foreach ($this->MetaProperties as $property => $content) {
+                $head .= '<meta property="' . $property . '" content="' . $content . '">';
+            }
+        }
+
+        $head .= '</head>';
+        return $head;
+    }
+
+    /**
+     * @param string $title
+     * @return $this
+     */
+    public function setTitle($title)
+    {
+        $title = trim($title);
+        $this->Title = $title;
+        return $this;
+    }
+}
