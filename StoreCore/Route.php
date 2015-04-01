75ff20256751ab77bc2a35d88a11e5dd76ef926d
Ward van der Put <Ward.van.der.Put@gmail.com>
2015-04-01 10:54:04 +0200

    Add Route::getPath() method

--- StoreCore/Route.php
+++ StoreCore/Route.php
@@ -6,24 +6,21 @@ class Route
     private $Controller;
     private $Path;
 
+    /**
+     * @param string $path
+     * @param string $controller
+     * @return void
+     */
     public function __construct($path, $controller)
     {
         $this->setPath($path);
         $this->setController($controller);
     }
 
-    private function setPath($path)
-    {
-        $path = '/' . ltrim($path, '/');
-        $path = mb_strtolower($path, 'UTF-8');
-        $this->Path = $path;
-    }
-
-    private function setController($controller)
-    {
-        $this->Controller = $controller;
-    }
-    
+    /**
+     * @param void
+     * @return void
+     */
     public function dispatch()
     {
         $controller = $this->Controller;
@@ -33,5 +30,34 @@ class Route
         } else {
             $thread = new $controller();
         }
+    }
+
+    /**
+     * @param void
+     * @return string
+     */
+    public function getPath()
+    {
+        return $this->Path;
+    }
+
+    /**
+     * @param string $controller
+     * @return void
+     */
+    private function setController($controller)
+    {
+        $this->Controller = $controller;
+    }
+
+    /**
+     * @param string $path
+     * @return void
+     */
+    private function setPath($path)
+    {
+        $path = '/' . ltrim($path, '/');
+        $path = mb_strtolower($path, 'UTF-8');
+        $this->Path = $path;
     }
 }
