4673c73bb2f7b81a15a8591c5330d8b6133a1ba6
Ward van der Put <Ward.van.der.Put@gmail.com>
2015-04-08 11:28:30 +0200

    Implement Countable interface in LogFileManager

--- StoreCore/FileSystem/LogFileManager.php
+++ StoreCore/FileSystem/LogFileManager.php
@@ -1,8 +1,12 @@
 <?php
 namespace StoreCore\FileSystem;
 
-class LogFileManager
+class LogFileManager implements \Countable
 {
+    /** @type string VERSION */
+    const VERSION = '0.0.1';
+
+    /** @type array|bool $LogFiles */
     private $LogFiles;
 
     /**
@@ -18,7 +22,11 @@ class LogFileManager
     }
 
     /**
+     * Clear the logs directory.
+     *
      * @param bool $all
+     *     Delete all log files (true) or only empty log files (default false).
+     *
      * @return void
      */
     public function clear($all = false)
@@ -27,10 +35,32 @@ class LogFileManager
         foreach ($log_files as $key => $filename) {
             if (is_dir(STORECORE_FILESYSTEM_LOGS . $filename)) {
                 unset($log_files[$key]);
-            } elseif (filesize(STORECORE_FILESYSTEM_LOGS . $filename) === 0) {
-                unlink(STORECORE_FILESYSTEM_LOGS . $filename);
+            } elseif ($filename === '.htaccess') {
                 unset($log_files[$key]);
+            } else {
+                $filename = str_ireplace('.log', null, $filename) . '.log';
+                if ($all === true || filesize(STORECORE_FILESYSTEM_LOGS . $filename) === 0) {
+                    if (unlink(STORECORE_FILESYSTEM_LOGS . $filename)) {
+                        unset($log_files[$key]);
+                    }
+                }
             }
+        }
+        $this->LogFiles = $log_files;
+    }
+
+    /**
+     * Count the number of log files.
+     *
+     * @param void
+     * @return int
+     */
+    public function count()
+    {
+        if (!is_array($this->LogFiles)) {
+            return 0;
+        } else {
+            return count($this->LogFiles);
         }
     }
 }
