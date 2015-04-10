209e510aafec6955fe63751d0b022eff81f5d41f
Ward van der Put <Ward.van.der.Put@gmail.com>
2015-04-10 17:16:29 +0200

    Add generic View class

---
+++ StoreCore/View.php
@@ -0,0 +1,56 @@
+<?php
+namespace StoreCore;
+
+class View
+{
+    private $Data = array();
+    private $Template;
+
+    /**
+     * @param null|string $template
+     * @return void
+     */
+    public function __construct($template = null)
+    {
+        if ($template !== null) {
+            $this->setTemplate($template);
+        }
+    }
+
+    /**
+     * @param string $template
+     * @return void
+     */
+    public function setTemplate($template)
+    {
+        $this->Template = $template;
+    }
+
+    /**
+     * @param array $values
+     * @return void
+     */
+    public function setValues(array $values)
+    {
+        foreach ($values as $name => $value) {
+            $this->Data[$name] = $value;
+        }
+    }
+
+    /**
+     * @param void
+     * @return string
+     */
+    public function render()
+    {
+        if (count($this->Data) !== 0) {
+            extract($this->Data);
+        }
+
+        ob_start();
+        include $this->Template;
+        $content = ob_get_contents();
+        ob_end_clean();
+        return $content;
+    }
+}
