aa092d3fddc8ab2b14547c11912497677a9073c1
Ward van der Put <Ward.van.der.Put@gmail.com>
2015-04-12 10:11:32 +0200

    Add ignored settings to configurator

--- StoreCore/Admin/Configurator.php
+++ StoreCore/Admin/Configurator.php
@@ -8,16 +8,33 @@ class Configurator
      *     Default settings from the global config.ini configuration file.
      */
     private $DefaultSettings = array(
-        'STORECORE_KILL_SWITCH' => false,
-        'STORECORE_MAINTENANCE_MODE' => false,
-        'STORECORE_STATISTICS' => true,
-        'STORECORE_NULL_LOGGER' => false,
+        'STORECORE_KILL_SWITCH' => '',
+        'STORECORE_MAINTENANCE_MODE' => '',
+        'STORECORE_STATISTICS' => 1,
+        'STORECORE_NULL_LOGGER' => '',
 
         'STORECORE_DATABASE_DRIVER' => 'mysql',
         'STORECORE_DATABASE_DEFAULT_HOST' => 'localhost',
         'STORECORE_DATABASE_DEFAULT_DATABASE' => 'test',
         'STORECORE_DATABASE_USERNAME' => 'root',
         'STORECORE_DATABASE_PASSWORD' => '',
+    );
+
+    /**
+     * @var array $IgnoredSettings
+     *     Settings that MAY be set manually in the config.ini configuration
+     *     file and settings that SHOULD NOT be overwritten by config.php.
+     */
+    private $IgnoredSettings = array(
+        'STORECORE_FILESYSTEM_CACHE' => true,
+        'STORECORE_FILESYSTEM_LIBRARY_ROOT' => true,
+        'STORECORE_FILESYSTEM_LOGS' => true,
+        'STORECORE_FILESYSTEM_STOREFRONT_ROOT' => true,
+
+        'STORECORE_VERSION' => true,
+        'STORECORE_MAJOR_VERSION' => true,
+        'STORECORE_MINOR_VERSION' => true,
+        'STORECORE_PATCH_VERSION' => true,
     );
 
     /**
@@ -35,8 +52,15 @@ class Configurator
         if (array_key_exists('user', $defined_constants)) {
             $defined_constants = $defined_constants['user'];
             foreach ($defined_constants as $name => $value) {
-                if (strpos($name, 'STORECORE_', 0) === 0) {
-                    if (strpos($name, 'STORECORE_I18N_', 0) !== 0) {
+                $name = strtoupper($name);
+                if (
+                    strpos($name, 'STORECORE_', 0) === 0
+                    && strpos($name, 'STORECORE_I18N_', 0) !== 0
+                ) {
+                    if (
+                        array_key_exists($name, $this->DefaultSettings) === false
+                        || $this->DefaultSettings[$name] !== $value
+                    ) {
                         $this->set($name, $value);
                     }
                 }
@@ -49,7 +73,9 @@ class Configurator
      */
     public function __set($name, $value)
     {
-        $this->set($name, $value);
+        if (array_key_exists($name, $this->IgnoredSettings) === false) {
+            $this->set($name, $value);
+        }
     }
 
     /**
@@ -58,7 +84,7 @@ class Configurator
      */
     public function save()
     {
-        $file = '<?php' . PHP_EOL;
+        $file = '<?php' . "\n";
         foreach ($this->Settings as $name => $value)
         {
             if (is_numeric($value)) {
@@ -69,9 +95,10 @@ class Configurator
                 } elseif (is_object($value)) {
                     $value = serialize($value);
                 }
+                $value = str_ireplace('\\', '\\\\', $value);
                 $file .= "define('{$name}', '{$value}');";
             }
-            $file .= PHP_EOL;
+            $file .= "\n";
         }
 
         $return = file_put_contents(STORECORE_FILESYSTEM_STOREFRONT_ROOT . 'config.php', $file, LOCK_EX);
