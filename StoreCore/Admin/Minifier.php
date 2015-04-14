f8eda932d239ae70cea0590bd527368f84e4abaf
Ward van der Put <Ward.van.der.Put@gmail.com>
2015-04-14 08:20:28 +0200

    Update admin CSS and minifier

--- StoreCore/Admin/Minifier.php
+++ StoreCore/Admin/Minifier.php
@@ -1,8 +1,22 @@
 <?php
 namespace StoreCore\Admin;
 
+/**
+ * Minify HTML or CSS
+ *
+ * @author    Ward van der Put <Ward.van.der.Put@gmail.com>
+ * @copyright Copyright (c) 2015 StoreCore
+ * @license   http://www.gnu.org/licenses/gpl.html GNU General Public License
+ * @package   StoreCore\Admin
+ * @version   0.1.0
+ */
 class Minifier
 {
+    /**
+     * @var string VERSION
+     */
+    const VERSION = '0.1.0';
+
     /**
      * @param string $text
      * @param bool $css
@@ -13,14 +27,15 @@ class Minifier
         $text = str_ireplace("\r\n", "\n", $text);
         $text = str_ireplace("\r", "\n", $text);
         $text = str_ireplace("\t", ' ', $text);
-        $text = str_ireplace("\n    ", "\n", $text);
-        $text = str_ireplace("\n  ", "\n", $text);
+        
+        $text = preg_replace('!\s+!', ' ', $text);
 
         if (stripos($text, '<pre') !== false) {
             $css = false;
         } else {
             $text = str_ireplace("\n", null, $text);
         }
+        $text = str_ireplace('> <', '><', $text);
 
         if ($css === true) {
             $text = str_ireplace(': ', ':', $text);
