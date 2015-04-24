0fbccd032bfa3281885823b3545c622140e801a3
Ward van der Put <Ward.van.der.Put@gmail.com>
2015-04-24 13:01:29 +0200

    Clean up database connection

--- StoreCore/Database/Connection.php
+++ StoreCore/Database/Connection.php
@@ -6,25 +6,30 @@ namespace StoreCore\Database;
  *
  * @author    Ward van der Put <Ward.van.der.Put@gmail.com>
  * @copyright Copyright (c) 2015 StoreCore
- * @license   http://www.gnu.org/licenses/gpl.html
+ * @license   http://www.gnu.org/licenses/gpl.html GNU General Public License
  * @package   StoreCore\Database
- * @version   0.0.1
+ * @version   0.0.2
  */
 class Connection extends \PDO
 {
-    /** @type string VERSION */
-    const VERSION = '0.0.1';
+    /**
+     * @var string VERSION
+     *   Semantic version (SemVer)
+     */
+    const VERSION = '0.0.2';
 
     /**
      * @param string $dsn
-     *     Optional data source name (DSN).  If the DNS is not supplied, it
-     *     defaults to the global StoreCore database configuration.  Using
-     *     additional DSNs allows for high scalability over multiple databases,
-     *     database servers, and other data sources.
+     *   Optional data source name (DSN).  If the DNS is not supplied, it
+     *   defaults to the global StoreCore database configuration.  Using
+     *   additional DSNs allows for high scalability over multiple databases,
+     *   database servers, and other data sources.
      *
      * @param string $username
      *
      * @param string $password
+     *
+     * @return void
      */
     public function __construct($dsn = null, $username = null, $password = null)
     {
@@ -34,40 +39,32 @@ class Connection extends \PDO
                 . ';host=' . \StoreCore\Database\DEFAULT_HOST
                 . ';charset=utf8';
         }
-  
+
         if ($username == null) {
             $username = \StoreCore\Database\DEFAULT_USERNAME;
             $password = \StoreCore\Database\DEFAULT_PASSWORD;
         }
 
+        // Force lower case column names and exceptions on errors.
+        $options = array(
+            \PDO::ATTR_CASE => \PDO::CASE_LOWER,
+            \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
+        );
+
         try {
-            parent::__construct($dsn, $username, $password);
-            $this->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
-            if (version_compare(PHP_VERSION, '5.3.6', '<')) {
-                $this->exec('SET NAMES utf8');
-            }
+            parent::__construct($dsn, $username, $password, $options);
         } catch (\PDOException $e) {
             $logger = new \StoreCore\FileSystem\Logger();
             $logger->error('Database connection failed: ' . trim($e->getMessage()));
-
-            // Reconnect after a few seconds to balance heavy loads
-            $seconds = mt_rand(2, 5);
-            $logger->notice('Reconnecting in ' . $seconds . ' seconds.');
-            sleep($seconds);
-            try {
-                parent::__construct($dsn, $username, $password);
-                $this->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
-                if (version_compare(PHP_VERSION, '5.3.6', '<')) {
-                    $this->exec('SET NAMES utf8');
-                }
-            } catch (\PDOException $e) {
-                $logger->critical('Service unavailable: ' . trim($e->getMessage()));
-                if (!headers_sent()) {
-                    header('HTTP/1.1 503 Service Unavailable', true, 503);
-                    header('Retry-After: 60');
-                }
-                exit;
+            if (!headers_sent()) {
+                header('HTTP/1.1 503 Service Unavailable', true, 503);
+                header('Retry-After: 60');
             }
+            exit;
+        }
+
+        if (version_compare(PHP_VERSION, '5.3.6', '<')) {
+            $this->exec('SET NAMES utf8 COLLATE utf8_general_ci');
         }
     }
 }
