85eb5446c7fc95cc52df609d07d4ea54b804ba8a
Ward van der Put <Ward.van.der.Put@gmail.com>
2015-05-14 18:15:00 +0200

    Add AbstractModel class to StoreCore\Database namespace

---
+++ StoreCore/Database/AbstractModel.php
@@ -0,0 +1,30 @@
+<?php
+namespace StoreCore\Database;
+
+/**
+ * Abstract MVC Model
+ *
+ * @author    Ward van der Put <Ward.van.der.Put@gmail.com>
+ * @copyright Copyright (c) 2015 StoreCore
+ * @license   http://www.gnu.org/licenses/gpl.html GNU General Public License
+ * @package   StoreCore\Database
+ * @version   0.1.0
+ *
+ * @uses \StoreCore\AbstractModel
+ *   This abstract model class extends the core abstract model by adding a
+ *   database connection.  Its main purpose therefore is to build MVC models
+ *   that require access to a database.
+ */
+abstract class AbstractModel extends \StoreCore\AbstractModel
+{
+    const VERSION = '0.1.0';
+
+    public function __construct(\StoreCore\Registry $registry)
+    {
+        $this->Registry = $registry;
+
+        if (false === $this->Registry->has('Connection')) {
+            $this->Registry->set('Connection', new \StoreCore\Database\Connection());
+        }
+    }
+}
