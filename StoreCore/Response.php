2e298b42232d9146777e6edeb633bc119ef354f3
Ward van der Put <Ward.van.der.Put@gmail.com>
2015-06-07 06:39:11 +0200

    Add response header and update compression

--- StoreCore/Response.php
+++ StoreCore/Response.php
@@ -8,25 +8,36 @@ namespace StoreCore;
  * @copyright Copyright (c) 2015 StoreCore
  * @license   http://www.gnu.org/licenses/gpl.html
  * @package   StoreCore
- * @version   0.0.1
+ * @version   0.1.0-alpha.1
  */
-class Response extends AbstractModel
+class Response extends AbstractController
 {
-    /** @type string VERSION */
-    const VERSION = '0.0.1';
+    const VERSION = '0.1.0-alpha.1';
 
     /**
-     * @type int               $CompressionLevel
-     * @type array             $Headers
-     * @type StoreCore\Request $Request
-     * @type string            $ResponseBody
+     * @type int    $CompressionLevel
+     * @type array  $Headers
+     * @type string $ResponseBody
      */
-    private $CompressionLevel = -1;
-    private $Headers;
-    private $Request;
-    private $ResponseBody;
+    protected $CompressionLevel = -1;
+    protected $Headers;
+    protected $ResponseBody;
 
     /**
+     * @param \StoreCore\Registry $registry
+     * @return void
+     */
+    public function __construct(\StoreCore\Registry $registry)
+    {
+        parent::__construct($registry);
+
+        if (defined('\\StoreCore\\RESPONSE_COMPRESSION_LEVEL')) {
+            $this->setCompression(\StoreCore\RESPONSE_COMPRESSION_LEVEL);
+        }
+    }
+
+    /**
+     * @api
      * @param string $header
      * @return void
      */
@@ -38,9 +49,10 @@ class Response extends AbstractModel
     /**
      * Create a gzip compressed string.
      *
+     * @internal
      * @param string $data
      * @param int $level
-     * @return string|bool
+     * @return string
      */
     private function compress($data, $level = -1)
     {
@@ -52,17 +64,6 @@ class Response extends AbstractModel
             return $data;
         }
 
-        if ($level < -1 || $level > 9) {
-            return $data;
-        }
-
-        if (connection_status()) {
-            return $data;
-        }
-
-        if ($this->Request == null) {
-            $this->Request = $this->Registry->get('Request');
-        }
         $accept_encoding = $this->Request->getAcceptEncoding();
         if (empty($accept_encoding)) {
             return $data;
@@ -72,8 +73,7 @@ class Response extends AbstractModel
             $encoding = 'x-gzip';
         } elseif (stripos($accept_encoding, 'gzip') !== false) {
             $encoding = 'gzip';
-        }
-        if (!isset($encoding)) {
+        } else {
             return $data;
         }
 
@@ -84,38 +84,35 @@ class Response extends AbstractModel
     /**
      * Output the full response.
      *
+     * @api
      * @param void
      * @return void
      */
     public function output()
     {
-        if ($this->ResponseBody) {
-            if ($this->CompressionLevel !== 0) {
-                $output = $this->compress($this->ResponseBody, $this->CompressionLevel);
-                if ($output !== false) {
-                    $this->setResponseBody($output);
-                    $this->setCompression(0);
-                }
-            }
+        if ($this->ResponseBody !== null && $this->CompressionLevel !== 0) {
+            $this->ResponseBody = $this->compress($this->ResponseBody, $this->CompressionLevel);
+            $this->setCompression(0);
         }
 
         if (!headers_sent()) {
-            foreach ($this->Headers as $header) {
-                header($header, true);
+            if ($this->Headers !== null) {
+                foreach ($this->Headers as $header) {
+                    header($header, true);
+                }
             }
+            header('X-Frame-Options: SAMEORIGIN', true);
             header('X-Powered-By: StoreCore/' . \StoreCore\VERSION, true);
             header('X-UA-Compatible: IE=edge', true);
         }
 
-        if ($this->ResponseBody) {
-            $request = $this->Registry->get('Request');
-            if ($request->getRequestMethod() !== 'HEAD') {
-                echo $this->ResponseBody;
-            }
+        if ($this->ResponseBody !== null && $this->Request->getRequestMethod() !== 'HEAD') {
+            echo $this->ResponseBody;
         }
     }
 
     /**
+     * @api
      * @param string $url
      * @param int $status
      * @return void
@@ -131,22 +128,39 @@ class Response extends AbstractModel
     /**
      * Set the response compression level.
      *
-     * @param int $level
-     *     The level of output compression, ranging from 0 for no compression
-     *     up to 9 for maximum compression.   If not given, the default
-     *     compression level will be the default compression level of the
-     *     zlib library.
+     * @api
+     *
+     * @param int|bool $level
+     *   The level of output compression, ranging from 0 for no compression up
+     *   to 9 for maximum compression.   If not given, the default compression
+     *   level will be the default compression level of the zlib library.
+     *   Compression may also be enabled/disabled with a true/false.
      *
      * @return void
      */
     public function setCompression($level = -1)
     {
-        $this->CompressionLevel = (int)$level;
+        if ($level === false) {
+            $level = 0;
+        } elseif ($level === true) {
+            $level = -1;
+        } elseif (!is_int($level)) {
+            throw new \InvalidArgumentException(__METHOD__ . ' expects parameter 1 to be an integer or boolean.');
+        }
+
+        if ($level < -1) {
+            $level = 1;
+        } elseif ($level > 9) {
+            $level = 9;
+        }
+
+        $this->CompressionLevel = $level;
     }
 
     /**
      * Set the response content.
      *
+     * @api
      * @param string $output
      * @return void
      */
