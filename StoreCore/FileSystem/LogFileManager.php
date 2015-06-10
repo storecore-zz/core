03641d811b019e3ae2a950c528b60fa5d25e4421
Ward van der Put <Ward.van.der.Put@gmail.com>
2015-06-10 10:54:15 +0200

    Add logs controller and view

--- StoreCore/FileSystem/LogFileManager.php
+++ StoreCore/FileSystem/LogFileManager.php
@@ -1,10 +1,18 @@
 <?php
 namespace StoreCore\FileSystem;
 
+/**
+ * Log File Manager
+ *
+ * @author    Ward van der Put <Ward.van.der.Put@gmail.com>
+ * @copyright Copyright (c) 2015 StoreCore
+ * @license   http://www.gnu.org/licenses/gpl.html GNU General Public License
+ * @package   StoreCore\Admin
+ * @version   0.1.0-alpha.1
+ */
 class LogFileManager implements \Countable
 {
-    /** @type string VERSION */
-    const VERSION = '0.0.1';
+    const VERSION = '0.1.0-alpha.1';
 
     /** @type array|bool $LogFiles */
     private $LogFiles;
@@ -25,28 +33,28 @@ class LogFileManager implements \Countable
      * Clear the logs directory.
      *
      * @param bool $all
-     *     Delete all log files (true) or only empty log files (default false).
+     *   Delete all log files (true) or only empty log files (default false).
      *
      * @return void
      */
     public function clear($all = false)
     {
-        $log_files = $this->LogFiles;
-        foreach ($log_files as $key => $filename) {
+        $files = $this->LogFiles;
+        foreach ($files as $key => $filename) {
             if (is_dir(\StoreCore\FileSystem\LOGS_DIR . $filename)) {
-                unset($log_files[$key]);
+                unset($files[$key]);
             } elseif ($filename === '.htaccess') {
-                unset($log_files[$key]);
+                unset($files[$key]);
             } else {
                 $filename = str_ireplace('.log', null, $filename) . '.log';
                 if ($all === true || filesize(\StoreCore\FileSystem\LOGS_DIR . $filename) === 0) {
                     if (unlink(\StoreCore\FileSystem\LOGS_DIR . $filename)) {
-                        unset($log_files[$key]);
+                        unset($files[$key]);
                     }
                 }
             }
         }
-        $this->LogFiles = $log_files;
+        $this->LogFiles = $files;
     }
 
     /**
@@ -59,8 +67,39 @@ class LogFileManager implements \Countable
     {
         if (!is_array($this->LogFiles)) {
             return 0;
+        }
+        return count($this->LogFiles);
+    }
+
+    /**
+     * Read all log files in descending order by date.
+     *
+     * @param void
+     * @return string|null
+     */
+    public function read()
+    {
+        $files = $this->LogFiles;
+        if (!is_array($files)) {
+            return null;
+        }
+        arsort($files);
+
+        $return = (string)null;
+        foreach ($files as $filename) {
+            $file_contents = file_get_contents(\StoreCore\FileSystem\LOGS_DIR . $filename, false);
+            if ($file_contents !== false && !empty($file_contents)) {
+                $return .= $file_contents . "\n";
+            }
+        }
+
+        $return = str_ireplace("\r\n", "\n", $return);
+        $return = str_ireplace("\n\n", "\n", $return);
+
+        if (empty($return)) {
+            return null;
         } else {
-            return count($this->LogFiles);
+            return rtrim($return);
         }
     }
 }
