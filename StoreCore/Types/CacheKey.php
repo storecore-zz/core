937a76429c651994e23f2fc2787994ff19a79d81
Ward van der Put <Ward.van.der.Put@gmail.com>
2015-12-17 14:25:52 +0100

    Add DocBlock

--- StoreCore/Types/CacheKey.php
+++ StoreCore/Types/CacheKey.php
@@ -1,6 +1,15 @@
 <?php
 namespace StoreCore\Types;
 
+/**
+ * Cache Key
+ *
+ * @author    Ward van der Put <Ward.van.der.Put@gmail.com>
+ * @copyright Copyright (c) 2015 StoreCore
+ * @license   http://www.gnu.org/licenses/gpl.html GNU General Public License
+ * @package   StoreCore\Core
+ * @version   0.1.0-alpha.1
+ */
 class CacheKey
 {
     const VERSION = '0.1.0-alpha.1';
@@ -10,6 +19,7 @@ class CacheKey
 
     /**
      * @param string $str
+     *   Case-insensitive string.
      */
     public function __construct($str)
     {
@@ -24,7 +34,7 @@ class CacheKey
     {
         return $this->get();
     }
-    
+
     /**
      * @param void
      * @return string
@@ -35,11 +45,18 @@ class CacheKey
     }
 
     /**
-     * @param string
+     * @param string $str
      * @return void
-     */  
+     * @throws \InvalidArgumentException
+     */
     private function set($str)
     {
+        if (!is_string($str)) {
+            throw new \InvalidArgumentException(
+                'Argument 1 passed to ' . __METHOD__ . '() must be of the type string, ' . gettype($str) . ' given'
+            );
+        }
+
         $str = mb_strtolower($str, 'UTF-8');
         $this->Key = md5($str);
     }
