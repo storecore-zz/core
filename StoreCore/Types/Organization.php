2283c1aeb0c05a04ef601d03e59c51679e926005
Ward van der Put <Ward.van.der.Put@gmail.com>
2016-10-12 08:12:06 +0200

    Add \StoreCore\Types\Organization::setLegalName() method

--- StoreCore/Types/Organization.php
+++ StoreCore/Types/Organization.php
@@ -42,6 +42,25 @@ class Organization extends Thing
     }
 
     /**
+     * Get the item name.
+     *
+     * @param void
+     * @return string|null
+     */
+    public function getName()
+    {
+        if (array_key_exists('name', $this->Data)) {
+            return $this->Data['name'];
+        } elseif (array_key_exists('alternateName', $this->Data)) {
+            return $this->Data['alternateName'];
+        } elseif (array_key_exists('legalName', $this->Data)) {
+            return $this->Data['legalName'];
+        } else {
+            return null;
+        }
+    }
+
+    /**
      * Set the postal address of an organization.
      *
      * This method sets the postal address of the organization, which MAY be a
@@ -69,6 +88,21 @@ class Organization extends Thing
     public function setAggregateRating(AggregateRating $aggregate_rating)
     {
         $this->Data['aggregateRating'] = $aggregate_rating;
+        return $this;
+    }
+
+    /**
+     * Set the organization's official name.
+     *
+     * @param string
+     *   The official name of the organization, for example the registered
+     *   company name.
+     *
+     * @return $this
+     */
+    public function setLegalName($legal_name)
+    {
+        $this->setStringProperty('legalName', $legal_name);
         return $this;
     }
 
