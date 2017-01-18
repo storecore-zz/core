2852e2b10148d267240fe52078bd58b283e22843
Ward van der Put <Ward.van.der.Put@gmail.com>
2017-01-18 10:02:06 +0100

    Add PHPDoc

--- StoreCore/AbstractModel.php
+++ StoreCore/AbstractModel.php
@@ -5,27 +5,42 @@ namespace StoreCore;
  * Abstract MVC Model
  *
  * @author    Ward van der Put <Ward.van.der.Put@gmail.com>
- * @copyright Copyright (c) 2015 StoreCore
+ * @copyright Copyright © 2015-2017 StoreCore
  * @license   http://www.gnu.org/licenses/gpl.html GNU General Public License
  * @package   StoreCore\Core
- * @version   0.1.0
+ * @version   1.0.0
  */
 abstract class AbstractModel
 {
-    const VERSION = '0.1.0';
+    /** @var string VERSION Semantic Version (SemVer) */
+    const VERSION = '1.0.0';
 
+    /** @var \StoreCore\Registry $Registry */
     protected $Registry;
 
+    /**
+     * @param \StoreCore\Registry $registry
+     * @return void
+     */
     public function __construct(\StoreCore\Registry $registry)
     {
         $this->Registry = $registry;
     }
 
+    /**
+     * @param string $key
+     * @return mixed
+     */
     public function __get($key)
     {
         return $this->Registry->get($key);
     }
 
+    /**
+     * @param string $key
+     * @param mixed $value
+     * @return void
+     */
     public function __set($key, $value)
     {
         $this->Registry->set($key, $value);
