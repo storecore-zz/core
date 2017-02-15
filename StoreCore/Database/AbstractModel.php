62f4a9566939a8391cf7d77663deb5d576d8f225
Ward van der Put <Ward.van.der.Put@gmail.com>
2017-02-15 13:30:11 +0100

    Refactor PDO MySQL database connections

--- StoreCore/Database/AbstractModel.php
+++ StoreCore/Database/AbstractModel.php
@@ -21,13 +21,39 @@ abstract class AbstractModel extends \StoreCore\AbstractModel
     const VERSION = '1.0.0';
 
     /**
+     * Add a database connection to the registry if none exists.
+     *
      * @param \StoreCore\Registry $registry
+     *   Global StoreCore registry.
+     *
      * @return void
+     *
+     * @throws \RuntimeException
+     *   A PDO exception is (re)thrown as an SPL runtime exception if
+     *   connecting to the default database fails or the proper connection
+     *   attributes could not fully be set.  If the database configuration
+     *   is set up correctly, the usual cause of a failed connection is a
+     *   “Too many connections” timeout, which can only be noticed at runtime.
      */
     public function __construct(\StoreCore\Registry $registry)
     {
         if (false === $registry->has('Connection')) {
-            $registry->set('Connection', new \StoreCore\Database\Connection());
+            try {
+                $dbh = new \StoreCore\Database\Connection();
+                if ($dbh === null) {
+                    throw new \RuntimeException();
+                }
+
+                if ($dbh->getAttribute(\PDO::ATTR_DRIVER_NAME) == 'mysql') {
+                    if (version_compare(PHP_VERSION, '5.3.6', '<')) {
+                        $dbh->exec('SET NAMES utf8 COLLATE utf8_general_ci');
+                    }
+                    $dbh->setAttribute(\PDO::ATTR_EMULATE_PREPARES, false);
+                }
+            } catch (\PDOException $e) {
+                throw new \RuntimeException($e->getMessage(), $e->getCode(), $e);
+            }
+            $registry->set('Connection', $dbh);
         }
         $this->Registry = $registry;
     }
