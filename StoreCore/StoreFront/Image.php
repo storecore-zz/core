1010f59fbd81115bcaf813ee0ca59e0addc90df7
Ward van der Put <Ward.van.der.Put@gmail.com>
2017-10-30 15:01:33 +0100

    Add Image class to StoreCore\StoreFront namespace

---
+++ StoreCore/StoreFront/Image.php
@@ -0,0 +1,17 @@
+<?php
+namespace StoreCore\StoreFront;
+
+/**
+ * Store Front Product Image
+ *
+ * @author    Ward van der Put <ward@storecore.org>
+ * @copyright Copyright © 2017 StoreCore
+ * @license   http://www.gnu.org/licenses/gpl.html GNU General Public License
+ * @package   StoreCore\CMS
+ * @version   0.1.0
+ */
+class Image
+{
+    /** @var string VERSION Semantic Version (SemVer) */
+    const VERSION = '0.1.0';
+}
