f6ca86315471a6abf4e87722ae4cd613b190a2f8
Ward van der Put <Ward.van.der.Put@gmail.com>
2017-11-01 13:57:37 +0100

    Add image width and height

--- StoreCore/StoreFront/Image.php
+++ StoreCore/StoreFront/Image.php
@@ -23,6 +23,18 @@ class Image
     private $Alt = '';
 
     /**
+     * @var int|null $Height
+     *   Image height in pixels.
+     */
+    private $Height;
+
+    /**
+     * @var int|null $Width
+     *   Image width in pixels.
+     */
+    private $Width;
+
+    /**
      * Convert the image object to an image HTML tag.
      *
      * @param void
@@ -34,6 +46,15 @@ class Image
     {
         $str  = '<img';
         $str .= ' alt="'. htmlentities($this->getAlt()) . '"';
+        
+        if ($this->getHeight() !== null) {
+            $str .= ' height="' . $this->getHeight() . '"';
+        }
+
+        if ($this->getWidth() !== null) {
+            $str .= ' width="' . $this->getWidth() . '"';
+        }
+
         $str .= '>';
         return $str;
     }
@@ -49,6 +70,34 @@ class Image
     public function getAlt()
     {
         return $this->Alt;
+    }
+
+    /**
+     * Get the image height.
+     *
+     * @param void
+     *
+     * @return int|null
+     *   Returns the image height in pixel as an integer or null if the image
+     *   height was not set.
+     */
+    public function getHeight()
+    {
+        return $this->Height;
+    }
+
+    /**
+     * Get the image width.
+     *
+     * @param void
+     *
+     * @return int|null
+     *   Returns the image width in pixels as an integer or null if the image
+     *   width was not set.
+     */
+    public function getWidth()
+    {
+        return $this->Width;
     }
 
     /**
@@ -72,5 +121,70 @@ class Image
         $alternative_text = strip_tags($alternative_text);
         $alternative_text = trim($alternative_text);
         $this->Alt = $alternative_text;
+    }
+
+    /**
+     * Set the image height.
+     *
+     * @param int $height_in_pixels
+     *   Image height in pixels (px).
+     *
+     * @return void
+     *
+     * @throws \DomainException
+     *   Throws a domain exception if the height is smaller than 1 or greater
+     *   than 2160, the default height of a 3840 × 2160 Ultra HD (UHD-1) or
+     *   “4K” image.
+     *
+     * @throws \InvalidArgumentException
+     *   Throws an invalid argument exception if the height is not a number.
+     */
+    public function setHeight($height_in_pixels)
+    {
+        if (!is_int($height_in_pixels)) {
+            if (is_numeric($height_in_pixels)) {
+                $height_in_pixels = (int)$height_in_pixels;
+            } else {
+                throw new \InvalidArgumentException();
+            }
+        }
+
+        if ($height_in_pixels < 1 || $height_in_pixels > 2160) {
+            throw new \DomainException();
+        }
+
+        $this->Height = $height_in_pixels;
+    }
+
+    /**
+     * Set the image width.
+     *
+     * @param int $width_in_pixels
+     *   Image width in pixels (px).
+     *
+     * @return void
+     *
+     * @throws \DomainException
+     *   Throws a domain exception if the width is smaller than 1 or greater
+     *   than 3840, the default width of a 3840 × 2160 Ultra HD image.
+     *
+     * @throws \InvalidArgumentException
+     *   Throws an invalid argument exception if the width is not a number.
+     */
+    public function setWidth($width_in_pixels)
+    {
+        if (!is_int($width_in_pixels)) {
+            if (is_numeric($width_in_pixels)) {
+                $width_in_pixels = (int)$width_in_pixels;
+            } else {
+                throw new \InvalidArgumentException();
+            }
+        }
+
+        if ($width_in_pixels < 1 || $width_in_pixels > 3840) {
+            throw new \DomainException();
+        }
+        
+        $this->Width = $width_in_pixels;
     }
 }
--- tests/StoreFront/ImageTest.php
+++ tests/StoreFront/ImageTest.php
@@ -39,6 +39,82 @@ class ImageTest extends PHPUnit_Framework_TestCase
     }
 
     /**
+     * @testdox Public __toString() method returns non-empty string
+     */
+    public function testPublicToStringMethodReturnsNonEmptyString()
+    {
+        $image = new \StoreCore\StoreFront\Image();
+        $this->assertFalse(empty($image->__toString()));
+        $this->assertFalse(empty((string)$image));
+        $this->assertTrue(is_string($image->__toString()));
+    }
+
+    /**
+     * @testdox Public getAlt() method exists
+     */
+    public function testPublicGetAltMethodExists()
+    {
+        $class = new \ReflectionClass('\StoreCore\StoreFront\Image');
+        $this->assertTrue($class->hasMethod('getAlt'));
+    }
+
+    /**
+     * @testdox Public getAlt() method is public
+     */
+    public function testPublicGetAltMethodIsPublic()
+    {
+        $method = new \ReflectionMethod('\StoreCore\StoreFront\Image', 'getAlt');
+        $this->assertTrue($method->isPublic());
+    }
+
+    /**
+     * @testdox Public getAlt() returns empty string by default
+     */
+    public function testPublicGetAltMethodReturnsEmptyStringByDefault()
+    {
+        $image = new \StoreCore\StoreFront\Image();
+        $this->assertTrue(empty($image->getAlt()));
+        $this->assertTrue(is_string($image->getAlt()));
+    }
+
+    /**
+     * @testdox Public getHeight() method exists
+     */
+    public function testPublicGetHeightMethodExists()
+    {
+        $class = new \ReflectionClass('\StoreCore\StoreFront\Image');
+        $this->assertTrue($class->hasMethod('getHeight'));
+    }
+
+    /**
+     * @testdox Public getHeight() method is public
+     */
+    public function testPublicGetHeightMethodIsPublic()
+    {
+        $method = new \ReflectionMethod('\StoreCore\StoreFront\Image', 'getHeight');
+        $this->assertTrue($method->isPublic());
+    }
+
+    /**
+     * @testdox Public getWidth() method exists
+     */
+    public function testPublicGetWidthMethodExists()
+    {
+        $class = new \ReflectionClass('\StoreCore\StoreFront\Image');
+        $this->assertTrue($class->hasMethod('getWidth'));
+    }
+
+    /**
+     * @testdox Public getWidth() method is public
+     */
+    public function testPublicGetWidthMethodIsPublic()
+    {
+        $method = new \ReflectionMethod('\StoreCore\StoreFront\Image', 'getWidth');
+        $this->assertTrue($method->isPublic());
+    }
+
+
+    /**
      * @testdox Public setAlt() method exists
      */
     public function testPublicSetAltMethodExists()
@@ -54,5 +130,61 @@ class ImageTest extends PHPUnit_Framework_TestCase
     {
         $method = new \ReflectionMethod('\StoreCore\StoreFront\Image', 'setAlt');
         $this->assertTrue($method->isPublic());
+    }
+
+    /**
+     * @testdox Public setHeight() method exists
+     */
+    public function testPublicSetHeightMethodExists()
+    {
+        $class = new \ReflectionClass('\StoreCore\StoreFront\Image');
+        $this->assertTrue($class->hasMethod('setHeight'));
+    }
+
+    /**
+     * @testdox Public setHeight() method is public
+     */
+    public function testPublicSetHeightMethodIsPublic()
+    {
+        $method = new \ReflectionMethod('\StoreCore\StoreFront\Image', 'setHeight');
+        $this->assertTrue($method->isPublic());
+    }
+
+    /**
+     * @expectedException \DomainException
+     * @testdox Public setHeight() method throws \DomainException on 0 (zero)
+     */
+    public function testPublicSetHeightMethodThrowsDomainExceptionOnZero()
+    {
+        $image = new \StoreCore\StoreFront\Image();
+        $image->setHeight(0);
+    }
+
+    /**
+     * @testdox Public setWidth() method exists
+     */
+    public function testPublicSetWidthMethodExists()
+    {
+        $class = new \ReflectionClass('\StoreCore\StoreFront\Image');
+        $this->assertTrue($class->hasMethod('setWidth'));
+    }
+
+    /**
+     * @testdox Public setWidth() method is public
+     */
+    public function testPublicSetWidthMethodIsPublic()
+    {
+        $method = new \ReflectionMethod('\StoreCore\StoreFront\Image', 'setWidth');
+        $this->assertTrue($method->isPublic());
+    }
+
+    /**
+     * @expectedException \DomainException
+     * @testdox Public setWidth() method throws \DomainException on 0 (zero)
+     */
+    public function testPublicSetHeightMethodThrowsDomainExceptionOnWidth()
+    {
+        $image = new \StoreCore\StoreFront\Image();
+        $image->setWidth(0);
     }
 }
