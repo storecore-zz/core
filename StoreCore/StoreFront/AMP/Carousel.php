73ec27aa54e901b32ca87d979a9ed87601bf634c
Ward van der Put <Ward.van.der.Put@gmail.com>
2017-11-08 16:45:22 +0100

    Add Carousel::setType() method

--- StoreCore/StoreFront/AMP/Carousel.php
+++ StoreCore/StoreFront/AMP/Carousel.php
@@ -14,7 +14,10 @@ namespace StoreCore\StoreFront\AMP;
  */
 class Carousel
 {
-    /** @var string VERSION Semantic Version (SemVer) */
+    /**
+     * @var string VERSION
+     *   Semantic Version (SemVer)
+     */
     const VERSION = '0.1.0';
 
     /**
@@ -22,4 +25,62 @@ class Carousel
      *   JavaScript source code that MUST be imported in the header for a carousel component.
      */
     const REQUIRED_SCRIPT = '<script async custom-element="amp-carousel" src="https://cdn.ampproject.org/v0/amp-carousel-0.1.js"></script>';
+
+    /**
+     * @var string TYPE_CAROUSEL
+     *   AMP carousel `type="carousel"` attribute (default).
+     */
+    const TYPE_CAROUSEL = 'carousel';
+
+    /**
+     * @var string TYPE_SLIDER
+     *   AMP `type="slides"` attribute turns a carousel into a slider.
+     */
+    const TYPE_SLIDES = 'slides';
+
+    /**
+     * @var string $Type
+     *   AMP carousel `type` HTML attribute, defaults to `carousel`.
+     */
+    private $Type = self::TYPE_CAROUSEL;
+
+    /**
+     * Create a carousel or slider.
+     *
+     * @param string $amp_carousel_type
+     *   Optional parameter to create a slider instead of a carousel (default).
+     *   If set to 'slides' a slider is created, otherwise a 'carousel'.  
+     *
+     * @return self
+     */
+    public function __construct($amp_carousel_type = self::TYPE_CAROUSEL)
+    {
+        $this->setType($amp_carousel_type);
+    }
+    
+    /**
+     * Set the carousel type.
+     *
+     * @param string $amp_carousel_type
+     *   Case-insensitive AMP carousel type `carousel` (default) or `slides`.
+     *
+     * @return void
+     *
+     * @throws \InvalidArgumentException
+     *   Throws an invalid argument logic exception if the carousel type is not
+     *   the string value `carousel` or `slides`.
+     */
+    public function setType($amp_carousel_type)
+    {
+        if (!is_string($amp_carousel_type)) {
+            throw new \InvalidArgumentException();
+        }
+        $amp_carousel_type = strtolower($amp_carousel_type);
+
+        if ($amp_carousel_type === self::TYPE_CAROUSEL || $amp_carousel_type === self::TYPE_SLIDES) {
+            $this->Type = $amp_carousel_type;
+        } else {
+            throw new \InvalidArgumentException();
+        }
+    }
 }
--- tests/StoreFront/AMP/AMPCarouselTest.php
+++ tests/StoreFront/AMP/AMPCarouselTest.php
@@ -30,4 +30,42 @@ class AMPCarouselTest extends PHPUnit_Framework_TestCase
         $class = new \ReflectionClass('\StoreCore\StoreFront\AMP\Carousel');
         $this->assertTrue($class->hasConstant('REQUIRED_SCRIPT'));
     }
+
+    /**
+     * @testdox Class has Type property
+     */
+    public function testClassHasTypeProperty()
+    {
+        $this->assertClassHasAttribute('Type', '\StoreCore\StoreFront\AMP\Carousel');
+    }
+
+    /**
+     * @testdox Public setType() method exists
+     */
+    public function testPublicSetTypeMethodExists()
+    {
+        $class = new \ReflectionClass('\StoreCore\StoreFront\AMP\Carousel');
+        $this->assertTrue($class->hasMethod('setType'));
+    }
+
+    /**
+     * @testdox Public setType() method is public
+     */
+    public function testPublicSetTypeMethodIsPublic()
+    {
+        $method = new \ReflectionMethod('\StoreCore\StoreFront\AMP\Carousel', 'setType');
+        $this->assertTrue($method->isPublic());
+    }
+
+    /**
+     * @expectedException \InvalidArgumentException
+     * @testdox Public setType() method throws \InvalidArgumentException on empty string
+     */
+    public function testPublicSetTypeMethodThrowsInvalidArgumentExceptionOnEmptyString()
+    {
+        $empty_string = (string)null;
+        $carousel = new \StoreCore\StoreFront\AMP\Carousel();
+        $this->assertEmpty($empty_string);
+        $carousel->setType($empty_string);
+    }
 }
