b1f28a40a81424ea30be912e6366fbc0f891cd2d
Ward van der Put <Ward.van.der.Put@gmail.com>
2017-11-22 12:09:53 +0100

    Add unit tests for password salts

---
+++ tests/Database/SaltTest.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ * @coversDefaultClass \StoreCore\Database\Salt
+ */
+class SaltTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @group distro
+     */
+    public function testSaltClassFileExists()
+    {
+        $this->assertFileExists(STORECORE_FILESYSTEM_LIBRARY_ROOT_DIR . 'Database/Salt.php');
+    }
+
+    /**
+     * @group distro
+     * @testdox VERSION constant is defined
+     */
+    public function testVersionConstantIsDefined()
+    {
+        $class = new \ReflectionClass('\StoreCore\Database\Salt');
+        $this->assertTrue($class->hasConstant('VERSION'));
+    }
+
+    /**
+     * @testdox CHARACTER_SET constant is defined
+     */
+    public function testCharacterSetConstantIsDefined()
+    {
+        $class = new \ReflectionClass('\StoreCore\Database\Salt');
+        $this->assertTrue($class->hasConstant('CHARACTER_SET'));
+    }
+
+    /**
+     * @covers ::getInstance
+     * @testdox Public static getInstance() method exists
+     */
+    public function testPublicStaticGetInstanceMethodExists()
+    {
+        $class = new \ReflectionClass('\StoreCore\Database\Salt');
+        $this->assertTrue($class->hasMethod('getInstance'));
+    }
+
+    /**
+     * @covers ::getInstance
+     * @depends testPublicStaticGetInstanceMethodExists
+     * @testdox Public static getInstance() method is public
+     */
+    public function testPublicStaticGetInstanceMethodIsPublic()
+    {
+        $method = new \ReflectionMethod('\StoreCore\Database\Salt', 'getInstance');
+        $this->assertTrue($method->isPublic());
+    }
+
+    /**
+     * @covers ::getInstance
+     * @depends testPublicStaticGetInstanceMethodExists
+     * @testdox Public static getInstance() method is static
+     */
+    public function testPublicStaticGetInstanceMethodIsStatic()
+    {
+        $method = new \ReflectionMethod('\StoreCore\Database\Salt', 'getInstance');
+        $this->assertTrue($method->isStatic());
+    }
+
+    /**
+     * @covers ::getInstance
+     * @depends testPublicStaticGetInstanceMethodIsStatic
+     * @testdox Public static getInstance() method returns 255 characters by default
+     */
+    public function testPublicStaticGetInstanceMethodReturns255CharactersByDefault()
+    {
+        for ($i = 1; $i <= 10; $i++) {
+            $this->assertTrue(strlen(\StoreCore\Database\Salt::getInstance()) === 255);
+        }
+    }
+
+    /**
+     * @covers ::getInstance
+     * @depends testPublicStaticGetInstanceMethodIsStatic
+     * @testdox Public static getInstance() method returns at least 2 characters
+     */
+    public function testPublicStaticGetInstanceMethodReturnsAtLeast2Characters()
+    {
+        $this->assertTrue(strlen(\StoreCore\Database\Salt::getInstance()) >= 2);
+        $this->assertTrue(strlen(\StoreCore\Database\Salt::getInstance(1)) >= 2);
+        $this->assertTrue(strlen(\StoreCore\Database\Salt::getInstance(0)) >= 2);
+    }
+}
