1c0f9d052fbbbc6fe98f43434e1aa44b877d25a8
Ward van der Put <Ward.van.der.Put@gmail.com>
2018-05-12 17:05:00 +0200

    Implement StringableInterface

--- StoreCore/Types/StoreID.php
+++ StoreCore/Types/StoreID.php
@@ -5,14 +5,17 @@ namespace StoreCore\Types;
  * Store Identifier
  *
  * @author    Ward van der Put <Ward.van.der.Put@storecore.org>
- * @copyright Copyright © 2015-2017 StoreCore
+ * @copyright Copyright © 2015–2018 StoreCore™
  * @license   http://www.gnu.org/licenses/gpl.html GNU General Public License
  * @package   StoreCore\Core
  * @version   1.0.0
  */
-class StoreID extends TinyintUnsigned implements TypeInterface
+class StoreID extends TinyintUnsigned implements StringableInterface, TypeInterface
 {
-    /** @var string VERSION Semantic Version (SemVer) */
+    /**
+     * @var string VERSION
+     *   Semantic Version (SemVer).
+     */
     const VERSION = '1.0.0';
 
     /**
