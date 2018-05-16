49fbacec26539ab9333ff869485495e1a775e316
Ward van der Put <Ward.van.der.Put@gmail.com>
2018-05-16 08:07:04 +0200

    Add and update PHPDoc

--- StoreCore/Admin/LockScreen.php
+++ StoreCore/Admin/LockScreen.php
@@ -1,12 +1,29 @@
 <?php
 namespace StoreCore\Admin;
 
+/**
+ * Admin Lock Screen
+ *
+ * @author    Ward van der Put <Ward.van.der.Put@storecore.org>
+ * @copyright Copyright © 2015–2018 StoreCore™
+ * @license   http://www.gnu.org/licenses/gpl.html GNU General Public License
+ * @package   StoreCore\Core
+ * @version   0.1.0
+ */
 class LockScreen extends \StoreCore\AbstractController
 {
-    const VERSION = '0.1.0';
-    
     /**
+     * @var string VERSION
+     *   Semantic Version (SemVer).
+     */
+    const VERSION = '0.1.0';
+
+    /**
+     * Construct the lock screen.
+     *
      * @param \StoreCore\Registry $registry
+     *   Global StoreCore service locator.
+     *
      * @return void
      */
     public function __construct(\StoreCore\Registry $registry)
@@ -18,7 +35,7 @@ class LockScreen extends \StoreCore\AbstractController
             . '<h1><strong>Store</strong>Core<sup>™</sup></h1>'
             . '<p><a href="/admin/sign-in/" target="_top">' . \STORECORE\I18N\COMMAND_UNLOCK . '</a></p>'
             . '</div>';
-            
+
         $lock_screen = new \StoreCore\Admin\Document();
         $lock_screen->setTitle('StoreCore™');
         $lock_screen->addSection($html, '');
