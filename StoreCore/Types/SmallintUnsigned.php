e8135aabba1ce0a45a951f7fa93136b315117444
Ward van der Put <Ward.van.der.Put@gmail.com>
2018-06-02 13:47:23 +0200

    Add PHPDoc

--- StoreCore/Types/SmallintUnsigned.php
+++ StoreCore/Types/SmallintUnsigned.php
@@ -1,12 +1,48 @@
 <?php
 namespace StoreCore\Types;
 
+/**
+ * Unsigned Small Integer
+ *
+ * @author    Ward van der Put <Ward.van.der.Put@storecore.org>
+ * @copyright Copyright © 2015, 2018 StoreCore™
+ * @license   http://www.gnu.org/licenses/gpl.html GNU General Public License
+ * @package   StoreCore\Core
+ * @version   0.1.0
+ *
+ * @see https://dev.mysql.com/doc/refman/8.0/en/integer-types.html
+ *      Integer Types (Exact Value) in the MySQL 8.0 Reference Manual 
+ */
 class SmallintUnsigned implements TypeInterface
 {
+    /**
+     * @var string VERSION
+     *   Semantic Version (SemVer).
+     */
     const VERSION = '0.1.0';
 
+    /**
+     * @var int $Value
+     *   Current value of the integer.
+     */
     protected $Value;
 
+    /**
+     * Construct an unsigned small integer.
+     *
+     * @param int|mixed $initial_value
+     *   Initial value of the integer.
+     *
+     * @param bool $strict
+     *   Enforce strict type handling (default true) or not (false).
+     *
+     * @return self
+     *
+     * @throws \InvalidArgumentException
+     *   Throws an invalid argument exception if the initial value is not
+     *   an integer (in strict mode) or cannot be converted to an integer (if
+     *   the strict mode is disabled).
+     */
     public function __construct($initial_value, $strict = true)
     {
         if ($strict && !is_int($initial_value)) {
