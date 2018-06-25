5240e7adc1a1b18b232b50b29f49e82ba33b6909
Ward van der Put <Ward.van.der.Put@gmail.com>
2018-06-25 08:30:05 +0200

    Move Google Analytics (GA) out to separate namespace

--- StoreCore/Modules/Google/Analytics/EcommerceItemHit.php
+++ StoreCore/Modules/Google/Analytics/EcommerceItemHit.php
@@ -1,18 +1,21 @@
 <?php
-namespace StoreCore\StoreFront;
+namespace Google\Analytics;
 
 /**
  * Google Analytics E-commerce Item Hit
  *
  * @author    Ward van der Put <Ward.van.der.Put@storecore.org>
- * @copyright Copyright (c) 2016 StoreCore
+ * @copyright Copyright © 2016–2018 StoreCore™
  * @license   http://www.gnu.org/licenses/gpl.html GNU General Public License
  * @package   StoreCore\BI
  * @version   0.1.0
  */
-class GoogleAnalyticsEcommerceItemHit extends GoogleAnalyticsHit
+class EcommerceItemHit extends Hit
 {
-    /** @var string VERSION Semantic Version (SemVer) */
+    /**
+     * @var string VERSION
+     *   Semantic Version (SemVer).
+     */
     const VERSION = '0.1.0';
 
     /**
