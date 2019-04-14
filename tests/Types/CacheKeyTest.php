20b7607350142b3591862404531552d14622791e
Ward van der Put <Ward.van.der.Put@gmail.com>
2019-04-14 19:24:00 +0200

    Upgrade cache key to SHA-256

--- tests/Types/CacheKeyTest.php
+++ tests/Types/CacheKeyTest.php
@@ -3,9 +3,9 @@ class CacheKeyTest extends PHPUnit_Framework_TestCase
 {
     /**
      * @group distro
-     * @testdox Core cache key class file exists
+     * @testdox Cache key class file exists
      */
-    public function testCoreStoreIdClassFileExists()
+    public function testCacheKeyClassFileExists()
     {
         $this->assertFileExists(STORECORE_FILESYSTEM_LIBRARY_ROOT_DIR . 'Types' . DIRECTORY_SEPARATOR .  'CacheKey.php');
     }
@@ -20,11 +20,53 @@ class CacheKeyTest extends PHPUnit_Framework_TestCase
     }
 
     /**
+     * @group hmvc
+     * @testdox Cache key implements Stringable interface
+     */
+    public function testCacheKeyImplementsStringableInterface()
+    {
+        $cache_key = new \StoreCore\Types\CacheKey();
+        $this->assertInstanceOf(\StoreCore\Types\StringableInterface::class, $cache_key);
+    }
+
+    /**
+     * @depends testCacheKeyImplementsStringableInterface
+     * @group hmvc
+     * @testdox Stringable object is a non-empty string
+     */
+    public function testStringableObjectIsANonEmptyString()
+    {
+        $cache_key = new \StoreCore\Types\CacheKey();
+        $this->assertInternalType('string', (string)$cache_key);
+        $this->assertNotEmpty((string)$cache_key);
+    }
+
+
+    /**
      * @group distro
      */
     public function testVersionConstantIsDefined()
     {
         $class = new \ReflectionClass('\StoreCore\Types\CacheKey');
-        $this->assertTrue($class->hasConstant('VERSION'));
+        $this->assertInternalType('string', \StoreCore\Types\CacheKey::VERSION);
+    }
+
+    /**
+     * @depends testVersionConstantIsDefined
+     * @group distro
+     */
+    public function testVersionConstantIsNonEmptyString()
+    {
+        $this->assertNotEmpty(\StoreCore\Types\CacheKey::VERSION);
+        $this->assertInternalType('string', \StoreCore\Types\CacheKey::VERSION);
+    }
+
+    /**
+     * @depends testVersionConstantIsNonEmptyString
+     * @group distro
+     */
+    public function testVersionMatchesMasterBranch()
+    {
+        $this->assertGreaterThanOrEqual('0.1.0', \StoreCore\Types\CacheKey::VERSION);
     }
 }
