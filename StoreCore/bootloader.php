027cb76878ed858bd205482a78c7e426293b1c3e
Ward van der Put <Ward.van.der.Put@gmail.com>
2019-08-10 14:43:47 +0200

    Update bootloader version

--- StoreCore/bootloader.php
+++ StoreCore/bootloader.php
@@ -4,9 +4,9 @@
  *
  * @author    Ward van der Put <Ward.van.der.Put@storecore.org>
  * @copyright Copyright © 2015–2019 StoreCore™
- * @license   http://www.gnu.org/licenses/gpl.html GNU General Public License
+ * @license   https://www.gnu.org/licenses/gpl.html GNU General Public License
  * @package   StoreCore\Core
- * @version   1.0.0-alpha.1
+ * @version   1.0.0-beta.1
  */
 
 // Global script/request timer start
