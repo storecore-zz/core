a5120cdaa0e4452bc264f4c5b3bd0729a44c5a9f
Ward van der Put <Ward.van.der.Put@gmail.com>
2019-09-16 08:27:40 +0200

    Replace stripslashes() with recursive stripslashes_deep()

--- StoreCore/bootloader.php
+++ StoreCore/bootloader.php
@@ -31,6 +31,25 @@ if (!function_exists('mb_ucfirst')) {
     }
 }
 
+// Strip slashes on GPC arrays.
+// @see https://www.php.net/manual/en/function.stripslashes.php
+if (!function_exists('stripslashes_deep')) {
+    function stripslashes_deep($value)
+    {
+        $value = is_array($value) ? array_map('stripslashes_deep', $value) : stripslashes($value);
+        return $value;
+    }
+}
+
+if (
+    (function_exists('get_magic_quotes_gpc') && get_magic_quotes_gpc())
+    || (ini_get('magic_quotes_sybase') && (strtolower(ini_get('magic_quotes_sybase')) != 'off'))
+) {
+    $_GET = stripslashes_deep($_GET);
+    $_POST = stripslashes_deep($_POST);
+    $_COOKIE = stripslashes_deep($_COOKIE);
+}
+
 // Set the framework library root directory in case it was not set
 // (for example, if the bootloader was included/required directly).
 if (!defined('STORECORE_FILESYSTEM_LIBRARY_ROOT_DIR')) {
