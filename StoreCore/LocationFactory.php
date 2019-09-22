c9c17b66ec35c880c6010d06e553e94b067b4aa9
Ward van der Put <Ward.van.der.Put@gmail.com>
2019-09-22 17:14:37 +0200

    Add URL parser to LocationFactory::createUri()

--- StoreCore/LocationFactory.php
+++ StoreCore/LocationFactory.php
@@ -28,6 +28,14 @@ class LocationFactory implements UriFactoryInterface
     /**
      * Create a new URI.
      *
+     * Creates PSR-7 UriInterface objects from URL strings by parsing the format:
+     * 
+     *     scheme :// authority / path ? query # fragment
+     * 
+     * where authority is:
+     *
+     *    scheme :// [user-info@]host[:port] / path ? query # fragment
+     *
      * @param string $uri
      *   The URI to parse.
      *
@@ -47,30 +55,72 @@ class LocationFactory implements UriFactoryInterface
             throw new \InvalidArgumentException();
         }
 
-        $location = new Location($uri);
+        $location = new Location();
 
-        $lowercase_uri = mb_strtolower($uri, 'UTF-8');
+        // Scheme
+        if (strpos($uri, '://') !== false) {
+            $uri = explode('://', $uri, 2);
+            $location->setScheme($uri[0]);
+            $uri = $uri[1];
+        }
 
-        if (substr($lowercase_uri, 0, 8) === 'https://') {
-            $location->setScheme('https');
-        } elseif (substr($lowercase_uri, 0, 7) === 'http://') {
-            $location->setScheme('http');
+        // Authority
+        if (strpos($uri, '/') !== false) {
+            $uri = explode('/', $uri, 2);
+            $location->setAuthority($uri[0]);
+            $uri = $uri[1];
+        }
+
+        // Path
+        if (strpos($uri, '?') !== false) {
+            $uri = explode('?', $uri, 2);
+            $location->setPath($uri[0]);
+            $uri = $uri[1];
+        }
+
+        // Query string and fragment, if any
+        if (!empty($uri)) {
+            if (strpos($uri, '#') !== false) {
+                $uri = explode('#', $uri, 2);
+                $location->setQuery($uri[0]);
+                $location->setFragment($uri[1]);
+            } else {
+                $location->setQuery($uri);
+            }
         }
 
         return $location;
     }
 
     /**
-     * Create a location data object for the current location.
+     * Create a location data object for the current request.
      *
      * @param void
      *
      * @return \Psr\Http\Message\UriInterface
+     *   Instance of a PSR-7 Uniform Resource Identifier (URI).
+     *
+     * @throws \RuntimeException
+     *   This method creates a `Location` object with a `UriInterface` by
+     *   parsing `$_SERVER` parameters and therefore throws a runtime exception
+     *   if `$_SERVER` is not set or is empty.
      */
     public static function getCurrentLocation()
     {
-        $factory = new LocationFactory();
-        $location = $factory->createUri($_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']);
+        if (!isset($_SERVER) || empty($_SERVER)) {
+            throw new \RuntimeException();
+        }
+
+        if (isset($_SERVER['HTTP_HOST']) && isset($_SERVER['REQUEST_URI'])) {
+            $factory = new LocationFactory();
+            try {
+                $location = $factory->createUri($_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']);
+            } catch (\Exception $e) {
+                throw new \RuntimeException($e->getMessage(), $e->getCode(), $e);
+            }
+        } else {
+            throw new \RuntimeException();
+        }
 
         if (
             (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off')
