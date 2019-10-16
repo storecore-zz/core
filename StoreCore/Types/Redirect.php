af78e482da9de97fd3a2ac7eb166c17dab3c9592
Ward van der Put <Ward.van.der.Put@gmail.com>
2019-10-16 08:59:06 +0200

    Update StoreCore\Types\Redirect

--- StoreCore/Types/Redirect.php
+++ StoreCore/Types/Redirect.php
@@ -1,44 +1,67 @@
 <?php
 namespace StoreCore\Types;
 
+use StoreCore\Types\CacheKey;
+
+use Psr\Cache\AbstractCacheItem;
+use Psr\Cache\CacheItemInterface;
+use Psr\Cache\InvalidArgumentException;
+
 /**
  * Cacheable HTTP Redirect
  *
  * @author    Ward van der Put <Ward.van.der.Put@storecore.org>
- * @copyright Copyright (c) 2016 StoreCore
+ * @copyright Copyright © 2016–2019 StoreCore™
  * @license   https://www.gnu.org/licenses/gpl.html GNU General Public License
  * @package   StoreCore\CMS
  * @version   0.1.0
  */
-class Redirect extends \Psr\Cache\AbstractCacheItem implements \Psr\Cache\CacheItemInterface
+class Redirect extends AbstractCacheItem implements CacheItemInterface
 {
+    /**
+     * @var string VERSION
+     *   Semantic Version (SemVer).
+     */
     const VERSION = '0.1.0';
 
     /**
      * @var string $RedirectFromURL
-     * @var string $RedirectToURL
+     *   Origin URL to redirect from.
      */
     private $RedirectFromURL;
+
+    /**
+     * @var string $RedirectToURL
+     *   Destination URL to redirect to.
+     */
     private $RedirectToURL;
 
     /**
+     * Create a redirect.
+     *
      * @param string|null $from_url
+     *   Origin URL to redirect from.
+     *
      * @param string $to_url
-     * @return void
+     *   Destination URL to redirect to.
+     *
+     * @return self
      */
     public function __construct($from_url = null, $to_url = '/')
     {
         if ($from_url !== null) {
             $this->set(array($from_url => $to_url));
         }
-
-        // Default expiration is "access plus 1 year".
-        $this->expiresAfter(new \DateInterval('P1Y'));
-    }
+     }
 
     /**
+     * Get the redirect URLs.
+     * 
      * @param void
+     *
      * @return array
+     *   Returns a URL key/value pair with the origin URL as the key and the
+     *   destination URL as the value.
      */
     public function get()
     {
@@ -46,7 +69,7 @@ class Redirect extends \Psr\Cache\AbstractCacheItem implements \Psr\Cache\CacheI
     }
 
     /**
-     * Permanent redirect.
+     * Execute a permanent redirect.
      *
      * @param void
      * @return void
@@ -54,29 +77,49 @@ class Redirect extends \Psr\Cache\AbstractCacheItem implements \Psr\Cache\CacheI
     public function redirect()
     {
         // Cache publicly for 1 year (the HTTP maximum).
-        if ($this->ExpiresAt === null) {
-            header('Cache-Control: max-age=31536000, public', true);
+        if ($this->ExpiresAt === null && $this->ExpiresAfter === null) {
+            header('Cache-Control: public, max-age=31536000', true);
         }
 
         header('Location: ' . $this->RedirectToURL, true, 301);
-        exit();
+        header('X-Powered-By: StoreCore', true);
+        exit(0);
     }
 
     /**
+     * Set the redirect URLs.
+     *
      * @param array $value
-     * @return void
+     *   Redirect URLs as a key/value pair.
+     *
+     * @return $this
+     *
      * @throws \Psr\Cache\InvalidArgumentException
+     *   Throws a PSR 6 Cache invalid argument cache exception if the `$value`
+     *   is not an array or the array does not consist of a pair of strings.
+     *
      * @uses \StoreCore\Types\CacheKey
+     *   The StoreCore CacheKey type class is used to generate a cache key
+     *   that is derived from the source URL.  The cache key is case-insensitive
+     *   and ignores the protocol prefix in de source URL.
      */
     public function set($value)
     {
-        if (!is_array($value)) {
-            throw new \Psr\Cache\InvalidArgumentException();
+        if (!\is_array($value)) {
+            throw new InvalidArgumentException();
+        }
+
+        if (\count($value) !== 1) {
+            throw new InvalidArgumentException();
         }
 
         foreach ($value as $from_url => $to_url) {
-            if (!is_string($from_url) || !is_string($to_url)) {
-                throw new \Psr\Cache\InvalidArgumentException();
+            if (!\is_string($from_url) || empty($from_url)) {
+                throw new InvalidArgumentException();
+            }
+
+            if (!\is_string($to_url) || empty($to_url)) {
+                throw new InvalidArgumentException();
             }
         }
 
@@ -84,7 +127,9 @@ class Redirect extends \Psr\Cache\AbstractCacheItem implements \Psr\Cache\CacheI
         $this->RedirectFromURL = $from_url;
         $this->RedirectToURL = $to_url;
 
-        $cache_key = new \StoreCore\Types\CacheKey($from_url);
-        $this->Key = (string)$cache_key;
+        $cache_key = new CacheKey($from_url);
+        $this->Key = (string) $cache_key;
+
+        return $this;
     }
 }
